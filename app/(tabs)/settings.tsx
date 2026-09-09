a2ef78555fb9b4d202807854588f6b63823f8736
Mriskiali_ <muafariskiali1805@gmail.com>
2026-09-09 18:18:45 +0700

    fix: strip UTF-8 BOM from restored files

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,4 +1,4 @@
-﻿import { ThemeColors } from "@/constants/theme";
+import { ThemeColors } from "@/constants/theme";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { useTranslation } from "@/hooks/useTranslation";
 import { useAlertStore } from "@/store/useAlertStore";
